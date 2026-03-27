83f3cae5f06ebd27e80a35bdce40e2cc66d61e5d
Pawel Koziol <pp.koziol@gmail.com>
2026-03-27 11:04:45 +0100

    0.0.1 [chore] renaming
     name and author information

--- uci.go
+++ uci.go
@@ -83,8 +83,8 @@ func uciLoop() {
 
 		switch tokens[0] {
 		case "uci":
-			fmt.Println("id name Sungorus 1.4")
-			fmt.Println("id author Pablo Vazquez")
+			fmt.Println("id name Rodent V 0.0.0")
+			fmt.Println("id author Naman Thanki, Pawel Koziol, based on Sungorus by Pablo Vazquez")
 			fmt.Println("option name Hash type spin default 16 min 1 max 4096")
 			fmt.Println("option name Clear Hash type button")
 			fmt.Println("uciok")
